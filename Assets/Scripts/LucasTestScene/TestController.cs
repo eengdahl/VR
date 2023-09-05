6bdbf9625d22371d1cb9897d190863c8425c1b6b
LucasKarjel <lucas.karjel@gmail.com>
2023-09-05 10:06:38 +0200

    Update TestController.cs

--- Assets/Scripts/LucasTestScene/TestController.cs
+++ Assets/Scripts/LucasTestScene/TestController.cs
@@ -1,54 +1,54 @@
-using UnityEngine;
-using Alteruna;
+//using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
-{
-    private CharacterController controller;
-    private Transform cameraTransform;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    [SerializeField]
-    private float playerSpeed = 2.0f;
-    [SerializeField]
-    private float jumpHeight = 1.0f;
-    [SerializeField]
-    private float gravityValue = -9.81f;
+//[RequireComponent(typeof(CharacterController))]
+//public class PlayerController : MonoBehaviour
+//{
+//    private CharacterController controller;
+//    private Transform cameraTransform;
+//    private Vector3 playerVelocity;
+//    private bool groundedPlayer;
+//    [SerializeField]
+//    private float playerSpeed = 2.0f;
+//    [SerializeField]
+//    private float jumpHeight = 1.0f;
+//    [SerializeField]
+//    private float gravityValue = -9.81f;
 
-    private void Awake()
-    {
-        controller = GetComponent<CharacterController>();
-        cameraTransform = Camera.main.transform;
-    }
-    private void Start()
-    {
-        Camera.main.transform.SetParent(transform);
-        Camera.main.transform.position = transform.position;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+//    private void Awake()
+//    {
+//        controller = GetComponent<CharacterController>();
+//        cameraTransform = Camera.main.transform;
+//    }
+//    private void Start()
+//    {
 
-    void Update()
-    {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+//        Camera.main.transform.SetParent(transform);
+//        Camera.main.transform.position = transform.position;
+//        Cursor.visible = false;
+//        Cursor.lockState = CursorLockMode.Locked;
+//    }
 
-        Vector2 movement = inputManager.GetPlayerMovement();
-        Vector3 move = new Vector3(movement.x, 0, movement.y);
-        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
-        move.y = 0;
-        controller.Move(playerSpeed * Time.deltaTime * move);
+//    void Update()
+//    {
+//        groundedPlayer = controller.isGrounded;
+//        if (groundedPlayer && playerVelocity.y < 0)
+//        {
+//            playerVelocity.y = 0f;
+//        }
 
-        // Changes the height position of the player..
-        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+        
+//        Vector3 move = new Vector3(movement.x, 0, movement.y);
+//        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+//        move.y = 0;
+//        controller.Move(playerSpeed * Time.deltaTime * move);
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
-}
+//        // Changes the height position of the player..
+//        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+//        {
+//            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+//        }
+
+//        playerVelocity.y += gravityValue * Time.deltaTime;
+//        controller.Move(playerVelocity * Time.deltaTime);
+//    }
+//}
