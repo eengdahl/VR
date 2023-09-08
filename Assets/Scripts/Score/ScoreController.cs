b1eaf4e65405d1e21e45b054c6b973099ec51e8d
AndStyver <andsty0125@skola.goteborg.se>
2023-09-08 11:07:56 +0200

    score controller takes in highscores
    not saved in prefs yet!!

--- Assets/Scripts/Score/ScoreController.cs
+++ Assets/Scripts/Score/ScoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Carl;
 
 public class ScoreController : MonoBehaviour
 {
@@ -21,7 +22,11 @@ public class ScoreController : MonoBehaviour
     public bool timeTrialEnabled;
 
     public GameController gameController;
-    
+
+    [SerializeField] List<int> easyHighscores = new(3);
+    [SerializeField] List<int> mediumHighscores = new(3);
+    [SerializeField] List<int> hardHighscores = new(3);
+
     private void Update()
     {
         if (multiplierTimer > 0)
@@ -67,5 +72,47 @@ public class ScoreController : MonoBehaviour
     public void ResetScore()
     {
         score = 0;
+    }
+
+    public void SaveHighscore(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                for (int i = 0; i < easyHighscores.Count; i++) //compare current score to current highscores
+                {
+                    if (score > easyHighscores[i]) //if it's higher, we insert it at that spot
+                    {
+                        easyHighscores.Insert(i, score);
+                        break;
+                    }
+                }
+                break;
+            case Difficulty.Normal:
+                for (int i = 0; i < mediumHighscores.Count; i++) //compare current score to current highscores
+                {
+                    if (score > mediumHighscores[i]) //if it's higher, we insert it at that spot
+                    {
+                        mediumHighscores.Insert(i, score);
+                        break;
+                    }
+                }
+                break;
+            case Difficulty.Hard:
+                for (int i = 0; i < hardHighscores.Count; i++) //compare current score to current highscores
+                {
+                    if (score > hardHighscores[i]) //if it's higher, we insert it at that spot
+                    {
+                        hardHighscores.Insert(i, score);
+                        break;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        if (easyHighscores.Count >= 4) //if we inserted, the list is too long and we trim the excess
+            easyHighscores.RemoveAt(3);
     }
 }
