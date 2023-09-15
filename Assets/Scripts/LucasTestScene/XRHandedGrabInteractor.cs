94e0cc718279910245eb759d1f3d605299c326ec
LucasKarjel <lucas.karjel@gmail.com>
2023-09-15 09:55:14 +0200

    New grabinteraction script

---
+++ Assets/Scripts/LucasTestScene/XRHandedGrabInteractor.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRHandedGrabInteractable : XRGrabInteractable
+{
+    [SerializeField]
+    private Transform LeftHandAttachTransform;
+    [SerializeField]
+    private Transform RightHandAttachTransform;
+    [SerializeField]
+    XRDirectInteractor LeftController;
+    [SerializeField]
+    XRDirectInteractor RightController;
+
+    private Transform m_OriginalAttachTransform;
+    [SerializeField]
+    private Transform m_attachTransform;
+
+    void Awake()
+    {
+        m_OriginalAttachTransform = m_attachTransform;
+    }
+
+    //  OnSelectEntering - set attachTransform - then call base
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject == LeftController)
+        {
+            Debug.Log($"Left hand");
+            attachTransform.SetPositionAndRotation(LeftHandAttachTransform.position, LeftHandAttachTransform.rotation);
+        }
+        else if (args.interactorObject == RightController)
+        {
+            Debug.Log($"Right hand");
+            attachTransform.SetPositionAndRotation(RightHandAttachTransform.position, RightHandAttachTransform.rotation);
+        }
+        else
+        {
+            // Handle case where interactor is not left hand or right hand (socket?)
+            attachTransform.SetPositionAndRotation(m_OriginalAttachTransform.position, m_OriginalAttachTransform.rotation);
+        }
+        base.OnSelectEntering(args);
+    }
+}
