8e8217c636369a065aebcc1362f2525efe8e533b
LucasKarjel <lucas.karjel@gmail.com>
2023-09-19 13:48:44 +0200

    Update CylinderPopulate.cs

--- Assets/Scripts/Weapons/CylinderPopulate.cs
+++ Assets/Scripts/Weapons/CylinderPopulate.cs
@@ -72,26 +72,26 @@ public class CylinderPopulate : MonoBehaviour
         float lefthandCylinderRelease = this.lefthandReleaseCylinder.action.ReadValue<float>();
 
         //button held, release it
-        //if (righthandCylinderRelease != 0)
-        //{
-        //    cylinderRB.isKinematic = false;
+        if (righthandCylinderRelease != 0)
+        {
+            cylinderRB.isKinematic = false;
 
-        //}
-        ////cylinder returns to default spot and no input, lock it
-        //if (transform.localRotation.y > 0.5 && righthandCylinderRelease == 0)
-        //{
-        //    cylinderRB.isKinematic = true;
-        //}
+        }
+        //cylinder returns to default spot and no input, lock it
+        if (transform.localRotation.y > 0.5 && righthandCylinderRelease == 0)
+        {
+            cylinderRB.isKinematic = true;
+        }
 
-        //if (lefthandCylinderRelease != 0)
-        //{
-        //    cylinderRB.isKinematic = false;
+        if (lefthandCylinderRelease != 0)
+        {
+            cylinderRB.isKinematic = false;
 
-        //}
-        //if (transform.localRotation.y > 0.5 && lefthandCylinderRelease == 0)
-        //{
-        //    cylinderRB.isKinematic = true;
-        //}
+        }
+        if (transform.localRotation.y > 0.5 && lefthandCylinderRelease == 0)
+        {
+            cylinderRB.isKinematic = true;
+        }
 
         if (inputData.leftControllerVelocity.x > 1 || Input.GetKeyDown(KeyCode.F) && cylinderOpen)
         {
