d9acc5f8892289054d8f4372cb0494e214c07e5e
AndStyver <andsty0125@skola.goteborg.se>
2023-09-20 09:41:09 +0200

    made achievement reference sheet

---
+++ Assets/Scripts/Achievements/AchievementsList.cs
@@ -0,0 +1,8 @@
+/* A List of all achievements and what you need to do to unlock them.
+Don't think this will be anywhere in the game, but just for our reference
+
+---Shootable---
+ -Mouse-leum   
+    Find the tiny mausoleum and shoot it
+
+*/
