9a7663276acadff91d9a78d9df24adbfea4fcffc
CarlZeidler <113012261+CarlZeidler@users.noreply.github.com>
2023-09-27 11:52:01 +0200

    Cleaned up script litter

--- Assets/Scripts/Controllers/TargetTrailsRenderer.cs
+++ Assets/Scripts/Controllers/TargetTrailsRenderer.cs
@@ -11,18 +11,11 @@ public class TargetTrailsRenderer : MonoBehaviour
     private LineRenderer lineRenderer;
     public List<GameObject> activeLines = new();
 
-    // private void SetUpLines()
-    // {
-    //     foreach (var target in activeTargets)
-    //     {
-    //         Vector3 startPoisiton = Vector3.Lerp(transform.position, target.transform.position, .15f);
-    //         Vector3 endPosition = Vector3.Lerp(transform.position, target.transform.position, .8f);
-    //         target.GetComponent<ShootableTarget>().LineSetUp(startPoisiton, endPosition);
-    //     }
-    // }
-
     public void ShowLine(GameObject target, ShootableTarget.MonsterType type)
     {
+        //Shows lines to targets that have not been hit for a while.
+        //Called by ShootableTarget
+        
         GameObject trail;
         waveCount += Random.Range(-1, 1);
         amplitude += Random.Range(-.015f, .015f);
@@ -87,76 +80,12 @@ public class TargetTrailsRenderer : MonoBehaviour
 
     private void SetupLines()
     {
-        // GameObject trail;
-        // waveCount += Random.Range(-1, 1);
-        // amplitude += Random.Range(-.015f, .015f);
+        //Creates a reference to the trail renderer, done by the trail renderer itself at the start
+        //of the round.
         foreach (GameObject target in activeTargets)
         {
             target.GetComponent<ShootableTarget>().trailRenderer = this;
         }
-
-        // foreach (GameObject target in activeTargets)
-        // {
-        //     //Renders each line when the targets are placed
-        //     ShootableTarget.MonsterType type = target.GetComponentInParent<ShootableTarget>().monsterType;
-        //
-        //     //Decides look of the trail (just color for now)
-        //     switch (type)
-        //     {
-        //         case ShootableTarget.MonsterType.dracula:
-        //             trail = vampireTrail;
-        //             break;
-        //         case ShootableTarget.MonsterType.skeleton:
-        //             trail = standardTrailRendererPrefab;
-        //             break;
-        //         case ShootableTarget.MonsterType.zombie:
-        //             trail = standardTrailRendererPrefab;
-        //             break;
-        //         case ShootableTarget.MonsterType.boss:
-        //             trail = standardTrailRendererPrefab;
-        //             break;
-        //         default:
-        //             trail = standardTrailRendererPrefab;
-        //             break;
-        //     }
-        //
-        //     Vector3 startPoisiton = Vector3.Lerp(transform.position, target.transform.position, .15f);
-        //     Vector3 endPosition = Vector3.Lerp(transform.position, target.transform.position, .8f);
-        //     
-        //     //Comment the below line to make trails "fly" to target instead of snaking along the ground
-        //     endPosition = new Vector3(endPosition.x, .4f, endPosition.z);
-        //     GameObject newLine = Instantiate(trail, startPoisiton, Quaternion.identity);
-        //     LineRenderer lineRenderer = newLine.GetComponent<LineRenderer>();
-        //     int maxIndex = lineRenderer.positionCount;
-        //
-        //     Vector3[] points = new Vector3[maxIndex];
-        //
-        //     for (int i = 0; i < maxIndex; i++)
-        //     {
-        //         float t = i / (float)(maxIndex - 1);
-        //         Vector3 linePoint = Vector3.Lerp(startPoisiton, endPosition, t);
-        //
-        //         float offset = 0f;
-        //
-        //         for (int waveIndex = 0; waveIndex < waveCount; waveIndex++)
-        //         {
-        //             float waveOffset = Mathf.Sin(t * Mathf.PI * waveCount * waveIndex * Mathf.PI) * amplitude;
-        //
-        //             offset += waveOffset;
-        //         }
-        //
-        //         if (i % 2 != 0)
-        //             linePoint += transform.right * offset;
-        //         else
-        //             linePoint += -transform.right * offset;
-        //         //Comment out the below line to make lines "fly"
-        //         linePoint.y = 0.4f;
-        //         points[i] = linePoint;
-        //     }
-        //     lineRenderer.SetPositions(points);
-        //     activeLines.Add(newLine);
-        //
-        // }
     }
 
     public void PopulateList(List<GameObject> newList)
