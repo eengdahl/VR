62aba086841a810e616111f5d6ffbae65bac12d4
LucasKarjel <lucas.karjel@gmail.com>
2023-10-05 13:38:50 +0200

    Sounds

--- Assets/Scripts/Audio/monsterspawnSound.cs
+++ Assets/Scripts/Audio/monsterspawnSound.cs
@@ -16,7 +16,13 @@ public class monsterspawnSound : MonoBehaviour
         ass.pitch = Random.Range(0.8f, 1.2f);
         if (monster == ShootableTarget.MonsterType.skeleton) return skeletonClip;
         if (monster == ShootableTarget.MonsterType.dracula) return draculaClip;
-        if (monster == ShootableTarget.MonsterType.zombie) return zombieClip;
+        if (monster == ShootableTarget.MonsterType.zombie) 
+        {
+            ass.volume = 0.4f;
+                
+            return zombieClip;
+        } 
+
         if (monster == ShootableTarget.MonsterType.boss) return bossClip;
         if (monster == ShootableTarget.MonsterType.werewolf) return werewolfClip;
         else { return skeletonClip; }
